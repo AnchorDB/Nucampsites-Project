2f24c4be6546d60050ed333c7bf0bafceb273c6b
Dan Bellacero <Oceanside31@gmail.com>
2022-06-21 19:58:24 -0400

    NPM Scripts P1

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+$(function() {
+    $('.carousel').carousel ( { interval: 2000});
+    $('#carouselBtn').click(function(){
+        if ($("#carouselBtn").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselBtn").children("i").removeClass("fa-pause");
+            $("#carouselBtn").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselBtn").children("i").removeClass("fa-play");
+            $("#carouselBtn").children("i").addClass("fa-pause"); 
+        }
+    });
+});
